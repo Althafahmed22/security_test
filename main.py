be03dc34e55340855445283789743f6aa677025b
Althaf1166 <132194821+Althafahmed22@users.noreply.github.com>
2025-09-04 12:32:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 def get_user(username):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-    # ❌ Vulnerable: SQL Injection risk
+    # ❌ Vulnerable: SQL In\jection risk
     query = f"SELECT * FROM users WHERE username = '{username}'"
     cursor.execute(query)
     return cursor.fetchall()
