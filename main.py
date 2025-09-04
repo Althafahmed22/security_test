0882d7c12677dd9180224c066b4c1a06f7742a61
Althaf1166 <132194821+Althafahmed22@users.noreply.github.com>
2025-09-04 15:24:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sqlite3
 def get_user(username):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
-    # ❌ Vulnerable: SQL In\jection risk
+    user_name="Althaf"
+    password="Althafmd4321@"
     query = f"SELECT * FROM users WHERE username = '{username}'"
     cursor.execute(query)
     return cursor.fetchall()
